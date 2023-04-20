d2670757ea12f17bdcb5fb6aad9dac6163a249bc
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-19 21:35:49 -0300

    CSS da página

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs"
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTile] = useState("")
+  const [time, setTime] = useState("")
+  const [tarefas, setTarefas] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tarefa-header">
+        <h1>Tarefas React</h1>
+      </div>
+      <div className="form-tarefa">
+        <p>Formulário</p>
+      </div>
+      <div className="list-tarefa">
+        <p>Lista</p>
+      </div>
+      
     </div>
   );
 }
